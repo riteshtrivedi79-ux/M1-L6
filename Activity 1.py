18b717c6157acb8a7778abaf7dce1b6deb1e9241
riteshtrivedi79-ux <riteshtrivedi79@gmail.com>
2026-02-08 17:55:51 +0530

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,13 @@
+a=int(input("enter your number"))
+b=int(input("enter your number"))
+if a>0 and b>0 :
+    print("both the numbers are positive")
+else :
+    print("there must be a negative number")
+
+c=int(input("enter your number"))
+d=int(input("enter your number"))
+if a>0 or b>0 :
+    print("either of the number is greater than 0")
+else :
+    print("both are smaller than 0")
