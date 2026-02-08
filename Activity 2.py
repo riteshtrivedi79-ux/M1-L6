91fb5dc2b3cf397471a9328cae073c48b6ae954d
riteshtrivedi79-ux <riteshtrivedi79@gmail.com>
2026-02-08 18:07:35 +0530

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,19 @@
+a=3.5
+b=8.9
+if a != b :
+    print(a, "in not equal to", b)
+else :
+    print(a, "is equal to ",b)
+
+c=(input("type your first word - "))
+d=(input("enter your 2nd word - "))
+if c!=d :
+    print("the words are not same")
+else :
+    print("both of the words are same")
+
+e=int(input("enter your number"))
+if e%2!=0 :
+    print("the number is an odd number")
+else :
+    print ("the number is an even number")
